183327c4cd436fb3f4905f2564147b6beb0777ea
NAVJOT0111 <navjotkaur1440@gmail.com>
2025-01-25 15:38:26 +0530

    Add files via upload

---
+++ non_zero.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main(){
+     if(22.3){
+        printf("This is executed");
+     }
+     if("c"){
+        printf("This is also executed");
+     }
+     if(122){
+        printf("This is also executed");
+     }
+     return 0;
+}
