fa520d11b08baff82dc78a4d972608888b5c46fe
Navjot Kaur <navjotkaur1440@gmail.com>
2025-03-06 20:49:43 +0530

    New test code

--- fivedigit.c
+++ fivedigit.c
@@ -1,15 +1,17 @@
 #include <stdio.h> 
 
 int main(){
-     int num, digit, num2 = 0 ;
+     int num, digit, num2 = 0, place=1 ;
      printf("Enter a five digit number: ");
      scanf("%d", &num);
 
      while(num){
         digit = num%10;
-        digit += 1;
-        num2 = digit*10;
+        digit = (digit + 1) % 10;           // Add 1 and handle wrapping (9 + 1 = 0) 
+        num2 += digit * place;
+        place *= 10;
         num /= 10;
      }
+     printf("%d",num2);
      return 0;
 }
---
+++ loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    // for(intialisation; condition; statement that will execute after every loop)
+    int i;
+    for(i = 5; i ; i--){                     // i = true in case of conditon
+        printf("%d \n",i);
+    }
+}
