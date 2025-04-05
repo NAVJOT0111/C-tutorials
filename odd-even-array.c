16dc1504ec1eddbb9465dd135ef83dc244a0c5b4
Navjot Kaur <navjotkaur1440@gmail.com>
2025-04-05 14:43:53 +0530

    New Files

---
+++ odd-even-array.c
@@ -0,0 +1,32 @@
+// #include<stdio.h>
+// int main(){
+//     int i;
+//     int arr[7] = {1,2,3,4,5,6};
+//     for(i = 0; i<=6;i++){
+//         if(i%2 == 0){
+//             arr[i] = arr[i]*2;
+//         } else {
+//             arr[i] = arr[i] + 10;
+//         }
+//     }
+//     for (i = 0; i<=6; i++){
+//         printf("%d ", arr[i]);
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     int i, sum_odd = 0, sum_even=0,diff;
+//     int arr[7] = {1,2,3,4,5,6,7};
+//     for(i=0; i<=6;i++){
+//         if (i%2 == 0){
+//             sum_even += arr[i] ;
+//         } else{ sum_odd += arr[i];}
+//     }
+//     diff = sum_even - sum_odd;
+//     printf("%d",diff);
+//     return 0;
+// }
+
